26a4e5fd3784089d00b670c0e1ffae2efaae58ad
wyatthaas <wyatthaas@ou.edu>
2026-01-29 12:56:03 -0600

    Add files via upload

---
+++ multipleconditionals/MultipleConditionals/MultipleConditionals/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Please enter your membership type - Gold, Silver, or Guest:");
+string membershipType = Console.ReadLine();
+
+if (membershipType == "Gold")
+{
+    Console.WriteLine("Welcome, Gold Member!");
+}
+else if (membershipType == "Silver")
+{
+    Console.WriteLine("Welcome, Silver Member!");
+}
+else
+{
+    Console.WriteLine("Welcome, Guest!");
+}
+
+// using ternary operator to handle multiple conditions
+string message = membershipType == "Gold" ? "Welcome, Gold Member!" :
+    membershipType == "Silver" ? "Welcome Silver Member!" :
+   "Welcome, Guest!";
+Console.WriteLine(message);
+
+double discount = membershipType == "Gold" ? 0.20 :
+    membershipType == "Silver" ? 0.10 :
+    0.0;
+Console.WriteLine($"Your discount rate: {discount:P2}");
+
+double pre_discount_price = 100.0;
+double final_price = pre_discount_price * (1-discount);
+Console.WriteLine($"The final price is: {final_price:F2}");
